3543e7b7bd6a1625eb5b1302cfc10150a2609077
Shubhanjan Barai <shubhanjan_barai@persistent.com>
2022-05-02 19:35:48 +0530

    3rd Commit

---
+++ returnMagic.java
@@ -0,0 +1,16 @@
+import javax.annotation.processing.SupportedSourceVersion;
+
+public class returnMagic {
+    public static void main(String[] args) {
+        int a = 10, b = 16;
+        int c = sum(a,b);
+        System.out.println(c);
+    }
+    public static int sum(int a, int b) {
+        int n = a+b;
+        System.out.println("R");
+        System.out.println("D");
+        System.out.println("S");
+        return n;
+    }
+}

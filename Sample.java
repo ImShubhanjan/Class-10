3ac1bd7a8e7a28c7f75e9d2c65b6d318e5d7241a
Shubhanjan Barai <shubhanjan_barai@persistent.com>
2022-06-20 19:51:29 +0530

    20 June

--- Sample.java
+++ Sample.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 
 class Operation
 {
-    static int a, b, sumab, diffab;
+    static int a, b, sumab, diffab; //global varaible
 
     public static void accept() 
     {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter 1st: ");
-        a = sc.nextInt();
+         a = sc.nextInt();
         System.out.println("Enter 2nd: ");
-        b = sc.nextInt();
+         b = sc.nextInt();
     }
 
     public static void calculate() 
@@ -24,14 +24,22 @@ class Operation
         System.out.println("The Sum of a and b: " + sumab);
         System.out.println("The diff of a and b: " + diffab);
     }
+    public static void mango() 
+    {
+        System.out.println("mango");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(sumab);
+        System.out.println(diffab);
+    }
 }
 
 public class Sample
 {
     public static void main(String[] args) {
-        Operation obj = new Operation();
-        obj.accept();
-        obj.calculate();
-        obj.display();
+        // Operation obj = new Operation();
+        Operation.accept();
+        Operation.calculate();
+        Operation.display();
     }
 }
